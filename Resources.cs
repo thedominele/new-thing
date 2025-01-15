7a3aaa21ac0c74a50430c8695e9f70340e2b3cd7
thedominele <70078010+thedominele@users.noreply.github.com>
2025-01-15 13:02:42 +0100

    Add files via upload

---
+++ Resources.cs
@@ -0,0 +1,81 @@
+// NosAssistant2.GUI.resources (Embedded, Public, 10127 bytes)
+
+
+// NosAssistant2.GUIElements.BuffsetsMenu.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.ChangeNicknameForm.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.CharacterAdditionalSettings.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.ComboBoxItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.ControlPanelItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.DmgContributionCounterWindow.resources (Embedded, Public, 10127 bytes)
+
+
+// NosAssistant2.GUIElements.EnterLicenseWindow.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.FairyInfoItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.FamilyRecordsItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.GraphsItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.MarathonItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.MinimapWindow.resources (Embedded, Public, 10127 bytes)
+
+
+// NosAssistant2.GUIElements.MobFilterItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.MobFilterWindow.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.NAComboBox.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.NAMessageBox.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.RaiderPanelItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.RaidForm.resources (Embedded, Public, 10127 bytes)
+
+
+// NosAssistant2.GUIElements.RaidParticipantIntem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.RaidsHisotryItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.RaidsNotificationsForm.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.RaidsNotificationsItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.RaidStatusBarItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.RankingItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.RemindLicenseForm.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.SoundPanelItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.SoundsPanelForm.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.GUIElements.SPInfoItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.Top10MaxHitsItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.Top10TimesItem.resources (Embedded, Public, 180 bytes)
+
+// NosAssistant2.GUIElements.WaypointsConfigMenu.resources (Embedded, Public, 10580 bytes)
+
+
+// NosAssistant2.Properties.Resources.resources (Embedded, Public, 76765 bytes)
+
+
